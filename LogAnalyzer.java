fe2beb0a812d9d41d8637bdd1f24775af3bdbb11
David T <David.g.thompson2020@gmail.com>
2023-10-14 19:58:14 -0400

    Exercise 7.19

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -8,6 +8,10 @@ public class LogAnalyzer
 {
     // Where to calculate the hourly access counts.
     private int[] hourCounts;
+    // Where to calculate the daily access counts.
+    private int[] dayCounts;
+    // Where to calculate the monthly access counts.
+    private int[] monthCounts;
     // This will contain the name of the file to be analyzed
     private String file;
     // Use a LogfileReader to access the data.
@@ -22,6 +26,10 @@ public class LogAnalyzer
         // Create the array object to hold the hourly
         // access counts.
         hourCounts = new int[24];
+        // Creat an array to hold daily access counts
+        dayCounts = new int[28];
+        // Create an array to hold monthly access counts
+        monthCounts = new int[12];
         //Create a variable to store the name of the file
         this.file = file;
         // Create the reader to obtain the data.
@@ -38,6 +46,33 @@ public class LogAnalyzer
             int hour = entry.getHour();
             hourCounts[hour]++;
         }
+        reader.reset();
+    }
+    
+    /**
+     * Analyze the daily access data from the log file.
+     */
+    public void analyzeDailyData()
+    {
+        while(reader.hasNext()) {
+            LogEntry entry = reader.next();
+            int day = entry.getDay();
+            dayCounts[day - 1]++;
+        }
+        reader.reset();
+    }
+    
+    /**
+     * Analyze the monthly access data from the log file.
+     */
+    public void analyzeMonthlyData()
+    {
+        while(reader.hasNext()) {
+            LogEntry entry = reader.next();
+            int month = entry.getMonth();
+            monthCounts[month - 1]++;
+        }
+        reader.reset();
     }
 
     /**
@@ -54,12 +89,39 @@ public class LogAnalyzer
     }
     
     /**
+     * Print the daily counts.
+     * These should have been set with a prior
+     * call to analyzeDailyData.
+     */
+    public void printDailyCounts()
+    {
+        System.out.println("Day: Count");
+        for(int day = 0; day < dayCounts.length; day++) {
+            System.out.println((day + 1) + ": " + dayCounts[day]);
+        }
+    }
+    
+    /**
+     * Print the monthly counts.
+     * These should have been set with a prior
+     * call to analyzeMonthlyData.
+     */
+    public void printMonthlyCounts()
+    {
+        System.out.println("Month: Count");
+        for(int month = 0; month < monthCounts.length; month++) {
+            System.out.println((month + 1) + ": " + monthCounts[month]);
+        }
+    }
+    
+    /**
      * Print the lines of data read by the LogfileReader
      */
     public void printData()
     {
         reader.printData();
     }
+    
     /**
      * Return the number of accesses recorded in the log file.
      */
@@ -71,6 +133,7 @@ public class LogAnalyzer
         }
         return total;
     }
+    
     /**
      * Return the hour with the most enteries in it.
      */
@@ -86,6 +149,7 @@ public class LogAnalyzer
         }
         return busiest;
     }
+    
     /**
      * Return the hour with the least enteries in it.
      */
@@ -101,6 +165,7 @@ public class LogAnalyzer
         }
         return quietest;
     }
+    
     /**
      * Return the two-hour period with the most enteries. 
      * Only the first number will be returned
@@ -117,5 +182,96 @@ public class LogAnalyzer
             }
         }
         return busiest;
+    }
+    
+    /**
+     * Return the hour with the most enteries in it.
+     */
+    public int busiestDay()
+    {
+        int largestNum = 0;
+        int busiest = 0;
+        for(int day = 0; day < dayCounts.length; day++) {
+            if(dayCounts[day] >= largestNum){
+                largestNum = dayCounts[day];
+                busiest = day + 1;
+            }
+        }
+        return busiest;
+    }
+    
+    /**
+     * Return the hour with the most enteries in it.
+     */
+    public int busiestMonth()
+    {
+        int largestNum = 0;
+        int busiest = 0;
+        for(int month = 0; month < monthCounts.length; month++) {
+            if(monthCounts[month] >= largestNum){
+                largestNum = monthCounts[month];
+                busiest = month + 1;
+            }
+        }
+        return busiest;
+    }
+    
+    /**
+     * Return the hour with the least enteries in it.
+     */
+    public int quietestDay()
+    {
+        int smallestNum = dayCounts[0];
+        int quietest = 0;
+        for(int day = 0; day < dayCounts.length; day++) {
+            if(dayCounts[day] <= smallestNum){
+                smallestNum = dayCounts[day];
+                quietest = day + 1;
+            }
+        }
+        return quietest;
+    }
+    
+    /**
+     * Return the hour with the least enteries in it.
+     */
+    public int quietestMonth()
+    {
+        int smallestNum = monthCounts[0];
+        int quietest = 0;
+        for(int month = 0; month < monthCounts.length; month++) {
+            if(monthCounts[month] <= smallestNum){
+                smallestNum = monthCounts[month];
+                quietest = month + 1;
+            }
+        }
+        return quietest;
+    }
+    
+    /**
+     * Takes a month and returns how many enteries that month had.
+     * @param the month you want to check
+     * (I have no idea if this is what you wanted this method to be,
+     * since there's already a method above that shows how many 
+     * access there were each month. This just seemed like the next
+     * best thing to me.)
+     */
+    public int totalAccessesPerMonth(int month)
+    {
+        int enteries = monthCounts[month - 1];
+        return enteries;
+    }
+    
+    /**
+     * Calculates the average access per month.
+     */
+    public int averageAccessesPerMonth()
+    {
+        int sum = 0;
+        for(int month : monthCounts){
+            sum += month;
+        }
+        int average = sum / monthCounts.length;
+        return average;
     }
 }
